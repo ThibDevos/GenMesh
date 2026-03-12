3850008ee9823b12819df21f387ca531ed482b40
Thibaut pc13 <thibdevos@gmail.com>
2026-03-12 15:01:33 +0100

    int -> size_t

--- include/mesh/entity_view.h
+++ include/mesh/entity_view.h
@@ -20,7 +20,7 @@ struct entity_iterator
   using pointer           = value_type*;  
   using reference         = value_type&;
 
-  entity_iterator(Mesh * M_, int i, std::vector<int> const * idxs) : M(M_), idx(i), indices(idxs) {}
+  entity_iterator(Mesh * M_, int i, std::vector<size_t> const * idxs) : M(M_), idx(i), indices(idxs) {}
 
   value_type operator*() const {return entity_view<Mesh,D>(M, (*indices)[idx]);}
   pointer operator->() const {return entity_view<Mesh,D>(M, (*indices)[idx]);}
@@ -46,18 +46,18 @@ struct entity_iterator
   private:
     Mesh* M;
     size_t idx;
-    std::vector<int> const * indices;
+    std::vector<size_t> const * indices;
 };
 
 template<typename Mesh, size_t D>
 struct entity_range
 {
-  entity_range(Mesh * M_, std::vector<int> const * idxs) : M(M_), indices(idxs){}
+  entity_range(Mesh * M_, std::vector<size_t> const * idxs) : M(M_), indices(idxs){}
   entity_iterator<Mesh,D> begin(){return entity_iterator<Mesh,D>(M,0, indices);}
   entity_iterator<Mesh,D> end(){return entity_iterator<Mesh,D>(M,indices->size(), indices);}
   private:
     Mesh* M;
-    const std::vector<int> * indices;
+    const std::vector<size_t> * indices;
 
 };
 
@@ -68,7 +68,7 @@ template <typename Mesh, size_t D>
 class entity_view
 {
   public:
-    entity_view(Mesh * M_, size_t i) : M(M_), index_(i) {std::cout<<"Entity has dim "<<D<<"\n";}
+    entity_view(Mesh * M_, size_t i) : M(M_), index_(i) {}
 
     size_t index(){return index_;}
 
@@ -78,13 +78,13 @@ class entity_view
       if constexpr(D==d)
       {
         std::cout<<"Return adjacency  TODO\n";
-        std::vector<int> temp;
+        std::vector<size_t> temp;
         return entity_range<Mesh,d>(M, &temp);
       }
       else
       {
-        std::vector<int> & local_vertices = M->topo.template get_incidence<D,d>(index_);
-        return entity_range<Mesh,d>(M,&local_vertices);
+        std::vector<size_t> & local_indices= M->topo.template get_connectivities<D,d>(index_);
+        return entity_range<Mesh,d>(M,&local_indices);
       }
     }
     auto cells()     { return entities<Mesh::dim_topo>();   }
