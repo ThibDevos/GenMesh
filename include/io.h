a433247e954efbca525944ebdb76df8df3be0e8e
Thibaut pc13 <thibdevos@gmail.com>
2026-04-08 13:53:03 +0200

    add mesh file in error

--- include/io.h
+++ include/io.h
@@ -97,7 +97,7 @@ struct gmesh
 
     if (!f)
     {
-      throw std::runtime_error("mesh::read : file does not exist");
+      throw std::runtime_error("mesh::read : file " + meshfile + " does not exist");
     }
     getline(f, line);
     while (line != "$Nodes")
