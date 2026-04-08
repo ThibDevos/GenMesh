475014c698dbda2fafae9cae31336fbbce24ddf3
Thibaut pc13 <thibdevos@gmail.com>
2026-04-08 13:54:00 +0200

    add connectivities tests cell-vertex, vertex-cell

--- tests/mesh/main.cpp
+++ tests/mesh/main.cpp
@@ -1,5 +1,55 @@
-#include<iostream>
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+#include <mesh/mesh.h>
+#include <io.h>
+#include <core/log.h>
+#include <connectivities.h>
+
+using namespace bib;
+
+template <size_t D, size_t D1, size_t D2>
+void test_topology_connectivities(
+    topology<D> & topo,
+    std::vector<std::vector<size_t>> const & expected)
+{
+  assert(expected.size() == topo.nb_entities(D1));
+
+  for (size_t i = 0; i < expected.size(); ++i)
+  {
+    std::vector<size_t> const & result = topo.template get_connectivities<D1, D2>(i);
+    assert(result == expected[i]);
+  }
+}
+
 int main()
 {
-  std::cout<<"Hello world\n";
+  mesh<1> M1;
+  gmesh<mesh<1>> G1;
+  G1.read_gmsh(M1,std::string(TEST_FILES_DIR)+"/test_1d.msh");
+  message("Test 1D connectivities");
+  message("C1D_1_0");
+  test_topology_connectivities<1, 1, 0>(M1.topo, C1D_1_0);
+  message("C1D_0_1");
+  test_topology_connectivities<1, 0, 1>(M1.topo, C1D_0_1);
+  
+  mesh<2> M2;
+  gmesh<mesh<2>> G2;
+  message("Test 2D connectivities");
+  message("C2D_2_0");
+  G2.read_gmsh(M2,std::string(TEST_FILES_DIR)+"/test_2d.msh");
+  test_topology_connectivities<2, 2, 0>(M2.topo, C2D_2_0);
+  message("C2D_0_2");
+  test_topology_connectivities<2, 0, 2>(M2.topo, C2D_0_2);
+  
+  mesh<3> M3;
+  gmesh<mesh<3>> G3;
+  message("Test 3D connectivities");
+  message("C3D_3_0");
+  G3.read_gmsh(M3,std::string(TEST_FILES_DIR)+"/test2_3d.msh");
+  test_topology_connectivities<3, 3, 0>(M3.topo, C3D_3_0);
+  message("C3D_0_3");
+  test_topology_connectivities<3, 0, 3>(M3.topo, C3D_0_3);
+  return 0;
 }
